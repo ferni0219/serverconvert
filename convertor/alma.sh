1f16387df42cb8d73adc606d5f92e2aa0e8bfc15
ferni0219 <76744894+ferni0219@users.noreply.github.com>
2021-03-15 22:08:19 -0600

    Add files via upload

---
+++ convertor/alma.sh
@@ -0,0 +1,329 @@
+#!/bin/bash
+# Description: EL to AlmaLinux migration script.
+# License: GPLv3.
+# Environment variables:
+#   ALMA_RELEASE_URL - almalinux-release package download URL.
+#   ALMA_PUBKEY_URL - RPM-GPG-KEY-AlmaLinux download URL.
+
+set -euo pipefail
+
+BASE_TMP_DIR='/root'
+OS_RELEASE_PATH='/etc/os-release'
+VERSION='0.1.5'
+
+# Reports a completed step using a green color.
+#
+# $1 - Message to print.
+report_step_done() {
+    local -r message="${1}"
+    printf '\033[0;32m%-70sOK\033[0m\n' "${message}"
+}
+
+# Reports a failed step using a red color.
+#
+# $1 - Message to print.
+# $2 - Additional information to show (optional).
+report_step_error() {
+    local -r message="${1}"
+    local -r trace="${2:-}"
+    printf '\033[0;31m%-70sERROR\033[0m\n' "${message}" 1>&2
+    if [[ -n "${trace}" ]]; then
+        echo "${trace}" | while read -r line; do
+            printf '    %s\n' "${line}" 1>&2
+        done
+    fi
+}
+
+# Prints program usage information.
+show_usage() {
+    echo -e 'Migrates an EL system to AlmaLinux\n'
+    echo -e 'Usage: almalinux-deploy.sh [OPTION]...\n'
+    echo '  -h, --help           show this message and exit'
+    echo '  -v, --version        print version information and exit'
+}
+
+# Terminates the program if it is not run with root privileges
+assert_run_as_root() {
+    if [[ $(id -u) -ne 0 ]]; then
+        report_step_error 'Check root privileges' \
+            'Migration tool must be run as root'
+        exit 2
+    fi
+    report_step_done 'Check root privileges'
+}
+
+# Terminates the program if UEFI Secure Boot is enabled
+assert_secureboot_disabled() {
+    local -r message='Check Secure Boot disabled'
+    if dmesg | grep -P 'secureboot:\s+Secure\s+boot\s+enabled' 1>/dev/null; then
+        report_step_error "${message}" 'Secure Boot is not supported yet'
+        exit 1
+    fi
+    report_step_done "${message}"
+}
+
+# Prints a system architecture.
+get_system_arch() {
+    uname -i
+}
+
+# Reads a variable value from /etc/os-release.
+#
+# $1 - variable name.
+#
+# Returns the variable value.
+get_os_release_var() {
+    local -r var="${1}"
+    local val
+    if ! val="$(grep -oP "^${var}=\"\K.*?(?=\")" "${OS_RELEASE_PATH}")"; then
+        echo "Error: ${var} is not found in ${OS_RELEASE_PATH}" >&2
+        exit 1
+    fi
+    echo "${val}"
+}
+
+# Prints control type and version.
+get_panel_info() {
+    local panel_type=''
+    local panel_version=''
+    local -r cpanel_file='/usr/local/cpanel/cpanel'
+    local -r plesk_file='/usr/local/psa/version'
+    if [[ -x "${cpanel_file}" ]]; then
+        panel_type='cpanel'
+        panel_version=$("${cpanel_file}" -V 2>/dev/null | grep -oP '^[\d.]+')
+    elif [[ -f "${plesk_file}" ]]; then
+        panel_type='plesk'
+        panel_version=$(grep -oP '^[\d.]+' "${plesk_file}" 2>/dev/null)
+    fi
+    echo "${panel_type} ${panel_version}"
+}
+
+# Terminates the program if a platform is not supported by AlmaLinux.
+#
+# $1 - Operational system id (ID).
+# $2 - Operational system version (VERSION_ID).
+# $3 - System architecture (e.g. x86_64).
+assert_supported_system() {
+    local -r os_id="${1}"
+    local -r os_version="${2}"
+    local -r arch="${3}"
+    if [[ ${arch} != 'x86_64' ]]; then
+        report_step_error "Check ${arch} architecture is supported"
+        exit 1
+    fi
+    if [[ ${os_version} -ne 8 ]]; then
+        report_step_error "Check EL${os_version} is supported"
+        exit 1
+    fi
+    if [[ ${os_id} != 'centos' ]]; then
+        report_step_error "Check ${os_id} operating system is supported"
+        exit 1
+    fi
+    report_step_done "Check ${os_id}-${os_version}.${arch} is supported"
+}
+
+# Terminates the program if a control panel is not supported by AlmaLinux.
+#
+# $1 - Control panel type.
+# $2 - Control panel version.
+assert_supported_panel() {
+    local -r panel_type="${1}"
+    local -r panel_version="${2}"
+    if [[ "${panel_type}" == 'cpanel' ]]; then
+        report_step_error 'cPanel is not supported yet'
+        exit 1
+    elif [[ "${panel_type}" == 'plesk' ]]; then
+        report_step_error 'Plesk is not supported yet'
+        exit 1
+    fi
+}
+
+# Returns a latest almalinux-release RPM package download URL.
+#
+# $1 - AlmaLinux major version (e.g. 8).
+# $2 - System architecture (e.g. x86_64).
+#
+# Prints almalinux-release RPM package download URL.
+get_release_file_url() {
+    local -r os_version="${1}"
+    local -r arch="${2}"
+    echo "${ALMA_RELEASE_URL:-https://repo.almalinux.org/almalinux/almalinux-release-latest-${os_version}.${arch}.rpm}"
+}
+
+# Downloads and installs the AlmaLinux public PGP key.
+#
+# $1 - Temporary directory path.
+install_rpm_pubkey() {
+    local -r tmp_dir="${1}"
+    local -r pubkey_url="${ALMA_PUBKEY_URL:-https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux}"
+    local -r pubkey_path="${tmp_dir}/RPM-GPG-KEY-AlmaLinux"
+    local -r step='Download RPM-GPG-KEY-AlmaLinux'
+    local output
+    if ! output=$(curl -f -s -S -o "${pubkey_path}" "${pubkey_url}" 2>&1); then
+        report_step_error "${step}" "${output}"
+        exit 1
+    else
+        report_step_done "${step}"
+    fi
+    rpm --import "${pubkey_path}"
+    report_step_done 'Import RPM-GPG-KEY-AlmaLinux to RPM DB'
+    rm -f "${pubkey_path}"
+}
+
+# Downloads almalinux-release package.
+#
+# $1 - almalinux-release package download URL.
+# $2 - Temporary directory path.
+#
+# Prints downloaded file path.
+download_release_file() {
+    local -r release_url="${1}"
+    local -r tmp_dir="${2}"
+    local -r release_path="${tmp_dir}/almalinux-release-latest.rpm"
+    local output
+    if ! output=$(curl -f -s -S -o "${release_path}" "${release_url}" 2>&1); then
+        report_step_error 'Download almalinux-release package' "${output}"
+        exit 1
+    fi
+    echo "${release_path}"
+}
+
+# Terminates the program if a given RPM package checksum/signature is invalid.
+#
+# $1 - RPM package path.
+assert_valid_package() {
+    local -r pkg_path="${1}"
+    local output
+    if ! output=$(rpm -K "${pkg_path}" 2>&1); then
+        report_step_error "Verify $(basename "${pkg_path}") package" \
+            "${output}"
+        exit 1
+    fi
+}
+
+# Recursively removes a given directory.
+#
+# $1 - Directory path.
+cleanup_tmp_dir() {
+    rm -fr "${1:?}"
+}
+
+# Converts a CentOS system to AlmaLinux
+#
+# $1 - almalinux-release RPM package path.
+migrate_from_centos() {
+    local -r release_path="${1}"
+    local to_remove=''
+    local alma_pkg=''
+    local output
+    # replace CentOS packages with almalinux-release and remove centos-specific
+    # packages
+    for pkg_name in centos-linux-release centos-gpg-keys centos-linux-repos \
+                    libreport-plugin-rhtsupport; do
+        if rpm -q "${pkg_name}" &>/dev/null; then
+            to_remove="${to_remove} ${pkg_name}"
+        fi
+    done
+    if [[ -n "${to_remove}" ]]; then
+        # shellcheck disable=SC2086
+        rpm -e --nodeps ${to_remove}
+        for pkg_name in ${to_remove}; do
+            report_step_done "Remove ${pkg_name} package"
+        done
+    fi
+    rpm -Uvh "${release_path}"
+    report_step_done 'Install almalinux-release package'
+    # replace GUI packages
+    for pkg_name in centos-backgrounds centos-logos centos-indexhtml; do
+        if rpm -q "${pkg_name}" &>/dev/null; then
+            alma_pkg="${pkg_name//centos/almalinux}"
+            rpm -e --nodeps "${pkg_name}"
+            report_step_done "Remove ${pkg_name} package"
+            if ! output=$(dnf install -y "${alma_pkg}" 2>&1); then
+                report_step_error "Install ${alma_pkg} package" "${output}"
+            fi
+            report_step_done "Install ${alma_pkg} package"
+        fi
+    done
+}
+
+# Executes the 'dnf distro-sync -y' command.
+#
+# Returns the dnf command exit code.
+distro_sync() {
+    local -r step='Run dnf distro-sync -y'
+    local ret_code=0
+    dnf distro-sync -y || {
+        ret_code=${?}
+        report_step_error "${step}. Exit code: ${ret_code}"
+    }
+    report_step_done "${step}"
+    return ${ret_code}
+}
+
+main() {
+    local arch
+    local os_version
+    local os_id
+    local release_url
+    local tmp_dir
+    local release_path
+    local panel_type
+    local panel_version
+    assert_run_as_root
+    assert_secureboot_disabled
+    arch="$(get_system_arch)"
+    os_version="$(get_os_release_var 'VERSION_ID')"
+    os_version="${os_version:0:1}"
+    os_id="$(get_os_release_var 'ID')"
+    assert_supported_system "${os_id}" "${os_version}" "${arch}"
+
+    read -r panel_type panel_version < <(get_panel_info)
+    assert_supported_panel "${panel_type}" "${panel_version}"
+
+    release_url=$(get_release_file_url "${os_version}" "${arch}")
+    tmp_dir=$(mktemp -d --tmpdir="${BASE_TMP_DIR}" .alma.XXXXXX)
+    # shellcheck disable=SC2064
+    trap "cleanup_tmp_dir ${tmp_dir}" EXIT
+    install_rpm_pubkey "${tmp_dir}"
+
+    release_path=$(download_release_file "${release_url}" "${tmp_dir}")
+    report_step_done 'Download almalinux-release package'
+
+    assert_valid_package "${release_path}"
+    report_step_done 'Verify almalinux-release package'
+
+    case "${os_id}" in
+    centos)
+        migrate_from_centos "${release_path}"
+        ;;
+    *)
+        report_step_error "Migrate ${os_id}: not supported"
+        exit 1
+        ;;
+    esac
+
+    distro_sync || exit ${?}
+    printf '\n\033[0;32mMigration to AlmaLinux is completed\033[0m\n'
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    for opt in "$@"; do
+        case ${opt} in
+        -h | --help)
+            show_usage
+            exit 0
+            ;;
+        -v | --version)
+            echo "${VERSION}"
+            exit 0
+            ;;
+        *)
+            echo "Error: unknown option ${opt}" >&2
+            exit 2
+            ;;
+        esac
+    done
+
+    main
+fi
---
+++ convertor/convertor.py
@@ -0,0 +1,7 @@
+import subprocess
+
+
+print ("1. oracle linux 1")
+print ("2. alma linux 2")
+print ("3. Salir")
+print ("Elige una opcion")
